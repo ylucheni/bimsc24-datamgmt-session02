c0959554250e73fd55e712019610c85b269a57b7
David Leon <david.andres.leon@iaac.net>
2024-01-18 17:07:08 +0100

    Update App.vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<!-- the script is where the js code goes -->
+<script>
+
+
+
+</script>
+
+
+
+<!-- the template is where the html code goes -->
+<template>
+
+
+
+</template>
+
+
+
+<!-- style is where the css code goes -->
+<style scoped>
+
+
+</style>
