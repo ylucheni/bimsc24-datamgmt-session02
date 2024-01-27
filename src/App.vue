28d7461cc089a875b96a0f095a0e77d9d85b646a
ylucheni <yi-chen.lu@students.iaac.net>
2024-01-27 01:08:19 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,17 @@ import { ref } from "vue" // you need this to use ref()
 <!-- the template is where the html code goes -->
 <template>
 
+<div id="navbar" class="container">  
+        <div id="title">Yichen's bootcamp website</div>
+</div>
+
+<div id="flex">
+
+        <div id="sidebar" class="container"> Sidebar </div>
+
+        <div id="main" class="container"> Text input </div>
+
+</div>
 
 
 </template>
@@ -18,6 +29,77 @@ import { ref } from "vue" // you need this to use ref()
 
 <!-- style is where the css code goes -->
 <style scoped>
+
+div{
+    
+    background-color: rgb(59, 59, 59); 
+    color: white;
+}
+
+body{
+
+    margin:0;
+}
+
+div{
+
+    /* put the borders in the inside of container */
+    box-sizing: border-box;
+}
+
+img{
+
+    height: 100%;
+    width: auto;
+
+}
+
+#navbar{
+
+    height: 50px;
+    border-color: red;
+
+
+}
+
+#flex{
+    
+    display: flex;
+    height: calc(100vh - 50px);
+}
+
+#sidebar{
+    
+    width:30%;
+    border-color: blue;
+
+}
+
+#main{
+    width:70%;
+    border-color: green;
+
+}
+
+#title{
+    width: 70%;
+    float:left;
+}
+
+#logo{
+    width: 30%;
+    height: 100%;
+    float:right;
+    text-align: right;
+
+}
+
+.container{
+
+    border-style: dotted;
+    border-width: 1px;
+
+}
 
 
 </style>
